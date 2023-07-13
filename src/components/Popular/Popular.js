2173b1d12b0a883f0b4d5f651914c1f9ef2ce6a7
Vadim-Vatrya <VatryaVV@gmail.com>
2023-07-14 00:21:14 +0300

    add popular

---
+++ src/components/Popular/Popular.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useContext } from 'react';
+import { MovieContex } from '../../Contex/MovieContex';
+import Container from '../ReusableComponents/Container';
+
+const Popular = () => {
+  const { popularMovies } = useContext(MovieContex);
+
+  return (
+    <Div className="popularMovies">
+      <Container>
+        {popularMovies.results &&
+          popularMovies.results.map((popularMovieItem, index) => (
+            <img
+              key={index}
+              src={`https://image.tmdb.org/t/p/w400/${popularMovieItem.poster_path}`}
+              alt="poster"
+            />
+          ))}
+      </Container>
+    </Div>
+  );
+};
+
+const Div = styled.div`
+  width: 100%;
+  .container {
+    &:nth-child(1) {
+      height: 81vh;
+      overflow-y: scroll;
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+      &::-webkit-scrollbar {
+        width: 0;
+      }
+      .error {
+        font-size: 38px;
+        color: red;
+        height: 32px;
+      }
+    }
+    img {
+      width: 100%;
+      max-width: 240px;
+      height: 360px;
+      margin: 10px 0;
+    }
+  }
+  @media (max-width: 600px) {
+    .container {
+      img {
+        max-width: 100%;
+        height: 500px;
+      }
+    }
+  }
+  @media (min-width: 601px) and (max-width: 854px) {
+    .container {
+      img {
+        max-width: 48%;
+      }
+    }
+  }
+  @media (min-width: 855px) and (max-width: 1144px) {
+    .container {
+      img {
+        max-width: 31%;
+      }
+    }
+  }
+  @media (min-width: 1145px) and (max-width: 1365px) {
+    .container {
+      img {
+        max-width: 23.4%;
+      }
+    }
+  }
+`;
+
+export default Popular;
