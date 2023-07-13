945aa382efe22f7405e781601da57e5160ce6003
Vadim-Vatrya <VatryaVV@gmail.com>
2023-07-14 00:50:45 +0300

    fix problems

--- src/Contex/MovieContex.js
+++ src/Contex/MovieContex.js
@@ -1,20 +1,19 @@
-import { useState, createContext, useEffect, popularMovies } from 'react';
+import { useState, createContext, useEffect } from 'react';
 
 export const MovieContex = createContext();
 
 export const MovieState = ({ children }) => {
-  const API_KEY = '9d4fbae6d45a1f406cc115a66a4de03d';
+  const API_KEY = '0d4e8a72aec64d2db9275b816bb7cbd6';
 
   const [hiddenMenu, setHiddenMenu] = useState(true);
   const [activeLink, setActiveLink] = useState('Popular');
-  const [popularMovies, setPopularMpvies] = useState([]);
-
+  const [popularMovies, setPopularMovies] = useState([]);
   const getPopularMovies = async () => {
     const popularMoviesResponse = await fetch(
       `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=3`,
     );
     const popularMoviesData = await popularMoviesResponse.json();
-    setPopularMpvies(popularMoviesData);
+    setPopularMovies(popularMoviesData);
   };
 
   useEffect(() => {
