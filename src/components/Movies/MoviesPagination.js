2bf8fbc12a66afbd097fc7728a9159541e6ea224
Vadim-Vatrya <VatryaVV@gmail.com>
2023-07-16 23:52:47 +0300

    add styles

--- src/components/Movies/MoviesPagination.js
+++ src/components/Movies/MoviesPagination.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useContext } from 'react';
 import { MovieContext } from '../../Context/MovieContext';
+import styled from 'styled-components';
 
 import Container from '../ReusableComponents/Container';
 
 const MoviesPagination = () => {
-  const { newPage, showPagination } = useContext(MovieContext);
+  const { newPage, showPagination, currentPage } = useContext(MovieContext);
   return (
-    <div className="moviesPagination">
+    <Div className="moviesPagination">
       {showPagination && (
         <Container>
           <React.Fragment>
@@ -16,7 +17,24 @@ const MoviesPagination = () => {
           </React.Fragment>
         </Container>
       )}
-    </div>
+    </Div>
   );
 };
+
+const Div = styled.div`
+  idth: 100%;
+  min-height: calc(100vh - 80px);
+  display: flex;
+  > .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+  }
+  @media (max-width: 1365px) {
+    > .container {
+      max-width: 90%;
+    }
+  }
+`;
+
 export default MoviesPagination;
